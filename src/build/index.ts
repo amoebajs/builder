53d16f3791a34509b17083e621f2b25906c5be0f
Big Mogician <sunaoran@youzan.com>
2019-12-03 19:40:02 +0800

    fix: reject in buildSource

--- src/build/index.ts
+++ src/build/index.ts
@@ -5,12 +5,12 @@ export function buildSource(options: IOptions) {
   return new Promise((resolve, reject) => {
     webpack(fn(options), (err, stats) => {
       if (err) {
-        return Promise.reject({ type: "error", error: err });
+        return reject({ type: "error", error: err });
       }
       if (stats.hasErrors()) {
-        return Promise.reject({ type: "warning", error: stats.toString() });
+        return reject({ type: "warning", error: stats.toString() });
       }
-      return Promise.resolve();
+      return resolve();
     });
   });
 }
