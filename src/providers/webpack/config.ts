ffc331d3c632c3a4cbc66b7a42afdde40db767a4
Big Mogician <sunaoran@youzan.com>
2020-01-03 11:50:49 +0800

    fix: add slots for template

--- src/providers/webpack/config.ts
+++ src/providers/webpack/config.ts
@@ -4,6 +4,18 @@ import HtmlWebPackPlugin from "html-webpack-plugin";
 import { Injectable } from "../../core/decorators";
 import { Path } from "../path";
 
+export interface IWebpackTemplateScript {
+  type: "inline-javascript" | "src-javascript";
+  defer?: string | boolean;
+  async?: string | boolean;
+  value: string;
+}
+
+export interface IWebpackTemplateStyle {
+  type: "rel-stylesheet" | "inline-style";
+  value: string;
+}
+
 export interface IWebpackOptions {
   entry?: Partial<{
     app: string;
@@ -17,8 +29,8 @@ export interface IWebpackOptions {
     title: string;
     path: string;
     charset: string;
-    stylesheets: { href: string }[];
-    scripts: { src: string }[];
+    styles: IWebpackTemplateStyle[];
+    scripts: IWebpackTemplateScript[];
   }>;
   typescript?: Partial<{
     tsconfig: string;
@@ -33,11 +45,12 @@ export interface IWebpackOptions {
   }>;
 }
 
-const defaultScripts: any[] = [];
+const defaultScripts: IWebpackTemplateScript[] = [];
 
-const defaultStyleSheets = [
+const defaultStyleSheets: IWebpackTemplateStyle[] = [
   {
-    href:
+    type: "rel-stylesheet",
+    value:
       "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
   }
 ];
@@ -114,17 +127,45 @@ export class WebpackConfig {
         new HtmlWebPackPlugin({
           template:
             options.template?.path ??
-            this.path.resolve(__dirname, "..", "assets", "index.html"),
+            this.path.resolve(__dirname, "..", "..", "assets", "index.html"),
           title: options.template?.title ?? "Index",
           charset: options.template?.charset ?? "utf-8",
-          stylesheets: (options.template?.stylesheets ?? defaultStyleSheets)
-            .map(sheet => `<link rel="stylesheet" href="${sheet.href}"/>`)
-            .join("\n"),
-          scriptList: (options.template?.scripts ?? defaultScripts)
-            .map(script => `<script src="${script.src}"></script>`)
+          styleList: (options.template?.styles ?? defaultStyleSheets)
+            .map(style => createStyle(style, "    "))
             .join("\n")
+            .slice(4),
+          scriptList: (options.template?.scripts ?? defaultScripts)
+            .map(script => createScript(script, "    "))
+            .join("\n")
+            .slice(4)
         })
       ].concat(options.plugins ?? [])
     };
+  }
+}
+
+function createStyle(style: IWebpackTemplateStyle, block = "") {
+  switch (style.type) {
+    case "rel-stylesheet":
+      return `${block}<link rel="stylesheet" href="${style.value}"/>`;
+    case "inline-style":
+      return `${block}<style>\n${style.value}\n${block}</style>`;
+    default:
+      return block;
+  }
+}
+
+function createScript(script: IWebpackTemplateScript, block = "") {
+  const deferToken = script.defer ? `defer="${script.defer}"` : undefined;
+  const asyncToken = script.async ? `async="${script.async}"` : undefined;
+  let adds = [deferToken, asyncToken].filter(i => !!i).join(" ");
+  adds = adds.length === 0 ? adds + " " : " " + adds + " ";
+  switch (script.type) {
+    case "src-javascript":
+      return `${block}<script type="text/javascript"${adds}src="${script.value}"></script>`;
+    case "inline-javascript":
+      return `${block}<script type="text/javascript"${adds}>\n${script.value}\n${block}</script>`;
+    default:
+      return block;
   }
 }
