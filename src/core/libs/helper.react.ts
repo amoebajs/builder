7b14f1b4b019f3e037d9ae76c89302ed36363d62
root <root@DESKTOP-BigMogician.localdomain>
2020-01-06 23:24:25 +0800

    fix: remove useless property ref

--- src/core/libs/helper.react.ts
+++ src/core/libs/helper.react.ts
@@ -1,44 +1,19 @@
 import ts from "typescript";
-import { PropertyRef, ReactVbRef, resolveSyntaxInsert } from "../base";
+import { resolveSyntaxInsert } from "../base";
 import { BasicHelper } from "./helper.basic";
 import { IJsxAttrs } from "../../utils";
 
 export class ReactHelper extends BasicHelper {
-  public getRef(name: string): ReactVbRef | null;
-  public getRef(name: string): PropertyRef | null;
-  public getRef(name: string) {
-    const ref = super.getRef(name);
-    if (ref instanceof ReactVbRef) {
-      return ref;
-    }
-    return null;
-  }
-
-  public resolveRef(name: string): ts.Expression | null {
-    const ref = this.getRef(name);
-    if (ref) {
-      if (ref.type === "props") {
-        return ts.createPropertyAccess(ts.createThis(), ref["expression"]);
-      }
-    }
-    return super.resolveRef(name);
-  }
-
-  public createObjectAttr(value: {
-    [prop: string]: number | string | boolean | ts.Expression;
-  }) {
-    const kvs: [
-      string,
-      string | number | boolean | ts.Expression
-    ][] = Object.keys(value).map(k => [k, value[k]]);
+  public createObjectAttr(value: { [prop: string]: number | string | boolean | ts.Expression }) {
+    const kvs: [string, string | number | boolean | ts.Expression][] = Object.keys(value).map(k => [k, value[k]]);
     return ts.createObjectLiteral(
       kvs.map(([n, v]) =>
         ts.createPropertyAssignment(
           ts.createIdentifier(n),
-          resolveSyntaxInsert(typeof v, v, (_, e) => e)
-        )
+          resolveSyntaxInsert(typeof v, v, (_, e) => e),
+        ),
       ),
-      true
+      true,
     );
   }
 
@@ -46,7 +21,7 @@ export class ReactHelper extends BasicHelper {
     tagName: string,
     types: ts.TypeNode[],
     attrs: IJsxAttrs,
-    children?: (ts.JsxChild | string)[]
+    children?: (ts.JsxChild | string)[],
   ) {
     return ts.createJsxElement(
       ts.createJsxOpeningElement(
@@ -62,35 +37,26 @@ export class ReactHelper extends BasicHelper {
                   ? ts.createStringLiteral(<string>attrs[k])
                   : ts.createJsxExpression(
                       undefined,
-                      resolveSyntaxInsert(
-                        typeof attrs[k],
-                        attrs[k],
-                        (t, e) => e
-                      )
-                    )
-              )
-            )
-        )
+                      resolveSyntaxInsert(typeof attrs[k], attrs[k], (t, e) => e),
+                    ),
+              ),
+            ),
+        ),
       ),
-      (children || []).map(i =>
-        typeof i === "string" ? ts.createJsxText(i) : i
-      ),
-      ts.createJsxClosingElement(ts.createIdentifier(tagName))
+      (children || []).map(i => (typeof i === "string" ? ts.createJsxText(i) : i)),
+      ts.createJsxClosingElement(ts.createIdentifier(tagName)),
     );
   }
 
   public resolvePropState(expression: string): ts.PropertyAccessExpression;
-  public resolvePropState(
-    expression: string,
-    type: "props" | "state"
-  ): ts.PropertyAccessExpression;
+  public resolvePropState(expression: string, type: "props" | "state"): ts.PropertyAccessExpression;
   public resolvePropState(
     expression: string,
     options: Partial<{
       type: "props" | "state";
       defaultValue: any;
       defaultCheck: "||" | "??";
-    }>
+    }>,
   ): ts.PropertyAccessExpression;
   public resolvePropState(expression: string, sec?: any) {
     let type: "props" | "state" = "props";
@@ -104,36 +70,21 @@ export class ReactHelper extends BasicHelper {
         defaultValue = sec.defaultValue;
       }
     }
-    let expr: ts.Expression = ts.createPropertyAccess(
-      ts.createThis(),
-      type + "." + expression.toString()
-    );
+    let expr: ts.Expression = ts.createPropertyAccess(ts.createThis(), type + "." + expression.toString());
     if (defaultValue !== null) {
       expr = ts.createBinary(
         expr,
-        defaultCheck === "||"
-          ? ts.SyntaxKind.BarBarToken
-          : ts.SyntaxKind.QuestionQuestionToken,
-        resolveSyntaxInsert(typeof defaultValue, defaultValue, (_, __) =>
-          ts.createStringLiteral(String(defaultValue))
-        )
+        defaultCheck === "||" ? ts.SyntaxKind.BarBarToken : ts.SyntaxKind.QuestionQuestionToken,
+        resolveSyntaxInsert(typeof defaultValue, defaultValue, (_, __) => ts.createStringLiteral(String(defaultValue))),
       );
     }
     return expr;
   }
 
-  public createImport(
-    modulePath: string,
-    names: Array<string | [string, string]> | string = []
-  ) {
+  public createImport(modulePath: string, names: Array<string | [string, string]> | string = []) {
     const ref = ts.createStringLiteral(modulePath);
     if (typeof names === "string") {
-      return ts.createImportDeclaration(
-        [],
-        [],
-        ts.createImportClause(ts.createIdentifier(names), undefined),
-        ref
-      );
+      return ts.createImportDeclaration([], [], ts.createImportClause(ts.createIdentifier(names), undefined), ref);
     } else if (names.length === 0) {
       return ts.createImportDeclaration([], [], undefined, ref);
     } else {
@@ -146,12 +97,12 @@ export class ReactHelper extends BasicHelper {
             names.map(s =>
               ts.createImportSpecifier(
                 Array.isArray(s) ? ts.createIdentifier(s[0]) : undefined,
-                ts.createIdentifier(Array.isArray(s) ? s[1] : s)
-              )
-            )
-          )
+                ts.createIdentifier(Array.isArray(s) ? s[1] : s),
+              ),
+            ),
+          ),
         ),
-        ref
+        ref,
       );
     }
   }
